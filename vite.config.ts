df56a6b96cc5a8cf498b4d6aaf84bb97452c22ea
ygkn <2000ygkn0713@gmail.com>
2022-01-29 04:39:35 +0900

    introduce vite

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+// vite.config.ts
+import preact from '@preact/preset-vite';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import { chromeExtension } from 'vite-plugin-chrome-extension';
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: 'src/manifest.json',
+    },
+  },
+  plugins: [preact(), chromeExtension()],
+});
